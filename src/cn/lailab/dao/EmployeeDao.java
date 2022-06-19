d0ffdb51b6ebe5056506ee22d981f3fc13ec7a02
qian <1694319867@qq.com>
2022-06-19 22:45:53 +0800

    v1.0 Employee增删改查功能实现

--- src/cn/lailab/dao/EmployeeDao.java
+++ src/cn/lailab/dao/EmployeeDao.java
@@ -1,66 +1,59 @@
 package cn.lailab.dao;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import cn.lailab.entity.Employee;
+import cn.lailab.sql.sqlHelper;
 
 /**
  * Employee操作
  */
-@WebServlet("/EmployeeDao")
-public class EmployeeDao extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EmployeeDao() {
-        super();
-    }
-
-	/**
-	 * GET请求
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 内容输出
-		PrintWriter out = response.getWriter();
-		// 获取操作
-		String operate = request.getParameter("operate");
-		// 定义返回字符
-		StringBuffer sb = new StringBuffer();
-		// 以下是复用数据
-		
-		// 操作判断
-		switch(operate) {
-			// 增加操作
-			case "add":
-				break;
-			// 删除操作
-			case "del":
-				break;
-			// 修改操作
-			case "update":
-				break;
-			// 查询操作
-			case "select":
-				break;
-			default :
-				out.println("没有这个功能");
-				break;
+public class EmployeeDao {
+	
+	// 增加操作
+	public static boolean addEmployee(Employee emp) throws Exception {
+		String sql = "INSERT INTO employee(e_name, e_username, e_gender, e_age, e_phone, e_department, e_position, e_salary)" + 
+				"VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+		int rst = sqlHelper.updateByPstmt(sql, emp.getName(), emp.getUsername(), emp.getGender(), emp.getAge(), emp.getPhone(), emp.getDapartment(), emp.getPosition(), emp.getSalary());
+		return rst == 1;
+	}
+	
+	// 删除操作
+	public static boolean deleteEmployeeById(int id) throws Exception {
+		String sql = "DELETE FROM employee" + 
+				"WHERE e_id = ?";
+		int rst = sqlHelper.updateByPstmt(sql, id);
+		return rst == 1;
+	}
+	
+	// 修改操作
+	public static boolean updateEmployee(Employee emp) throws Exception {
+		String sql = "UPDATE employee SET e_name=?, e_username=?, e_gender=?, e_phone=?, e_department=?, e_position=?, e_salary=?" + 
+				"WHERE e_id=?";
+		int rst = sqlHelper.updateByPstmt(sql, emp.getName(), emp.getUsername(), emp.getGender(), emp.getAge(), emp.getPhone(), emp.getDapartment(), emp.getPosition(), emp.getSalary(), emp.getId());
+		return rst == 1;
+	}
+	
+	// 查询操作
+	public static List<Employee> queryAllEmployee() throws Exception {
+		String sql = "SELECT e_id, e_name, e_username, e_gender, e_age, e_phone, e_department, e_position, e_salary FROM employee";
+		ResultSet rst = sqlHelper.query(sql);
+		List<Employee> emps = new ArrayList<Employee>();
+		while(rst.next()) {
+			Employee emp = new Employee();
+			emp.setId(rst.getInt(1));
+			emp.setName(rst.getString(2));
+			emp.setUsername(rst.getString(3));
+			emp.setGender(rst.getString(4));
+			emp.setAge(rst.getInt(5));
+			emp.setPhone(rst.getString(6));
+			emp.setDapartment(rst.getString(7));
+			emp.setPhone(rst.getString(8));
+			emp.setSalary(rst.getDouble(9));
+			emps.add(emp);
 		}
-		out.println(operate);
+		return emps;
 	}
-
-	/**
-	 * POST请求
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
 }
